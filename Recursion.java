238c91fff4de2b7b0a504964aa1fba8f09f0c857
abdul awal <awal@twintechsoft.com>
2024-04-10 22:39:47 +0600

    palindrom in java

--- Recursion.java
+++ Recursion.java
@@ -2,11 +2,24 @@ class Recursion
 {
     public static void main(String []args)
     {
-        System.out.println("My First Java Program.");
+
+
+        System.out.println(isPalindrome("AFBFA"));
     }
 
 
 
 
-    public static  boolean isPalindromlkfjdskfjkrjh
+    public static  boolean isPalindrome(String input){
+        // define the base-case / stopping condition
+        if(input.length() == 0 || input.length() == 1){
+            return  true;
+        }
+        // do some work to shrink the problem space
+        if(input.charAt(0) == input.charAt(input.length()-1)){
+            return isPalindrome(input.substring(1, input.length() -1));
+        }
+        // Additional base-case to handle non-palindromes
+        return  false;
+    }
 }

26436355670ead6d8ed8dcc1d8ecfa7d167862df
abdul awal <awal@twintechsoft.com>
2024-04-19 22:38:20 +0600

    merge sort file is added

--- fibonacci.dart
+++ fibonacci.dart
@@ -1,11 +1,17 @@
+//45:54 / 1:51:35
+
+
+
 void main(){
-  print("Result: ${fib(8)}");
+  print("Result: ${fib(5)}");
 }
 
 int fib(int n){
   if((n == 0) || (n==1)){
+    print('hit Return n = $n');
     return n;
   } else{
+    print(' n = $n');
     return fib(n-1) +fib(n-2);
   }
 

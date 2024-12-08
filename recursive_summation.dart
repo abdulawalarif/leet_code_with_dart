70e9ebb6ef5cf6ce47291d25a3da6b242e2b2d22
Abdul Awal <abdulawalarif0011@gmail.com>
2024-12-08 13:21:42 +0600

    Update recursive_summation.dart

--- recursive_summation.dart
+++ recursive_summation.dart
@@ -10,3 +10,11 @@ int recursiveSummation(int inputNumber){
   }
   return inputNumber + recursiveSummation(inputNumber - 1);
 }
+
+
+// This Dart program calculates the summation of all integers from a given number down to 1 using a recursive function. 
+// The recursiveSummation function takes an integer as input and adds it to the summation of the next lower integer, 
+// continuing this process until the input number is 1 or less, at which point the recursion stops. 
+// The result is printed to the console. For example, recursiveSummation(10) 
+
+//returns the sum 10 + 9 + 8 + … + 1 = 55 10+9+8+…+1=55.
